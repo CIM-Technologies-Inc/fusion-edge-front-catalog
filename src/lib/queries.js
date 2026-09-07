03ff39394e0f31e5e3827c84bd46c50478745883
patrick151505 <patrickanonuevo.developer@gmail.com>
2026-09-08 07:52:30 +0800

    dollar to peso

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -260,7 +260,7 @@ export function formatPrice(cents) {
   if (cents == null) return ''
   return (cents / 100).toLocaleString(undefined, {
     style: 'currency',
-    currency: 'USD',
+    currency: 'PHP',
   })
 }
 
